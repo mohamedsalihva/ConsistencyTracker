d52c449022e47abbb82e7ad4ea9b1502fd91519a
mohamed salih va <salihva777@gmail.com>
2026-02-13 12:50:14 +0530

    cors updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
