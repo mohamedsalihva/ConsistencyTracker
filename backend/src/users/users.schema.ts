9d48307eac5c689dff6d2a4852e0fed25573c220
mohamed salih va <salihva777@gmail.com>
2026-02-24 13:16:36 +0530

    schem afor worksapce added

--- backend/src/users/users.schema.ts
+++ backend/src/users/users.schema.ts
@@ -1,4 +1,8 @@
 import { Prop,Schema,SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+
+export type UserRole = 'manager' | 'member';
+export type  SubscriptionStatus = 'none' | 'pending' | 'active' | 'failed';
 
 @Schema({timestamps:true})
 
@@ -12,5 +16,15 @@ export class User{
 
     @Prop({required:true})
     password:string;
+
+    @Prop({type:String, enum: ['manager', 'member'], default: 'member'}) role: UserRole;
+    
+    @Prop({ type: String, enum:['none','pending','active','failed'],default: 'none'})SubscriptionStatus: SubscriptionStatus;
+
+    @Prop ({type: mongoose.Schema.Types.ObjectId, ref:'workspace', default: null})workspaceId?: string | null;
+    
+    @Prop({type:mongoose.Schema.Types.ObjectId, ref: 'User', default: null})managerId?: string | null;
+
+
 }
 export const UserSchema = SchemaFactory.createForClass(User);
---
+++ backend/src/workspace/workspace.schema.ts
@@ -0,0 +1,22 @@
+import { Prop,Schema,SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+
+@Schema({ timestamps: true})
+export class workspace {
+    @Prop({required: true})
+    name: string;
+
+    @Prop ({type: mongoose.Schema.Types.ObjectId, ref:'User', required: true})
+    ownerId: string;
+
+    @Prop({required:true,unique:true})
+    inviteCode: string;
+
+    @Prop({default:'Manager'})
+    managerLabel: string;
+
+    @Prop({default: 'Member'})
+    memberLabel: string;
+}
+
+export const WorkspaceSchema = SchemaFactory.createForClass(workspace);
